2f090ba12a8c7bdb3664d3b681d4a4633c9ef142
mithilesh <mithilesh@M039>
2016-10-13 17:34:12 +0530

    tes

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -2,7 +2,7 @@
    namespace HelloWorld;
    class SayHello {
       public static function world() {
-         return 'Hello Tejas your Extension is sucess!';
+         return 'Hello Tejas your Extension is sucess!2';
       }
    }
 ?>
