9ba5a3cb9d0f5141e49962c3bd765c5752651608
MITHU-PC\abc <mkphp2@gmail.com>
2018-08-24 15:56:44 +0530

    changes some comment.

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -2,7 +2,8 @@
    namespace HelloWorld;
    class SayHello {
       public static function world() {
-         return 'Hello Tejas, your first Extension is successfully run! Well Done';
+         return 'Hello Tejas, your Extension is successfully run! Well Done.
+		 This is yii2 extension.';
       }
    }
 ?>
